9a3670e3e93e7f07c4fba36675748c6e8b8b5b02
= <rahul.gupta4@emc.com>
2020-05-07 09:10:59 +0530

    Updated 14.js with Memoization and Recurrsive Function

--- 14.js
+++ 14.js
@@ -1,26 +1,38 @@
 var t0 = Date.now();
 
+const cache = {};
+
+const collaz_seq = n =>
+  cache[n] === undefined                               // No value stored in cache
+    ? n === 1                                          // Last call in recurrsive function
+      ? (cache[n] = 1)                                 // Set cache as 1
+      : n % 2 === 0                                    // Check for Even or Odd
+        ? (cache[n] = collaz_seq(n / 2) + 1)           // Store the value 
+        : (cache[n] = collaz_seq((3 * n + 1) / 2) + 2) // 
+    : cache[n];
+
+/* 
 const memo = {};
 
-const collaz_seq = (n) => {
-  if (memo[n] === undefined) {
+if (memo[n] === undefined) {
     if (n === 1) {
       memo[n] = 1;
     } else if (n % 2 === 0) {
       memo[n] = collaz_seq(n / 2) + 1;
     } else {
-      memo[n] = collaz_seq((3 * n + 1) / 2) + 2;
+      memo[n] = collaz_seq((3 * n + 1) / 2 ) + 2;
     }
   }  
   return memo[n];
 };
+*/
 
 let largest = 0,
   number = 0,
   r = 0;
 
 for (let i = 1; i < 1000000; i++) {
-  r = collaz_seq(i);
+  r = collaz_seq(i);                                        
   if (r > largest) {
     largest = r;
     number = i;
@@ -28,5 +40,10 @@ for (let i = 1; i < 1000000; i++) {
 }
 
 var t1 = Date.now();
-console.log('Took', (t1 - t0).toFixed(4), 'milliseconds to generate:', 'Number: '+ number, 'Largest Length: '+ largest);
-
+console.log(
+  "Took",
+  (t1 - t0).toFixed(4),
+  "milliseconds to generate:",
+  "Number: " + number,
+  "Largest Length: " + largest
+);
