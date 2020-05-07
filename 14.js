e67dc17a614d6ecf3c25107c72ee02a0eb309fe6
= <rahul.gupta4@emc.com>
2020-05-07 06:13:31 +0530

    Modified 14.js with Memoization

--- 14.js
+++ 14.js
@@ -1,16 +1,32 @@
-var t0 = Date.now()
+var t0 = Date.now();
 
-const result = (n, l=0) => n === 1 ? ++l : 
-      n %2 == 0 ? (l+=1) && result( (n / 2 ), l) : (l+=2) && result( ( (3* n + 1) / 2 ), l ) ;
+const memo = {};
 
-let largest = 0, number = 0, r = 0;
+const collaz_seq = (n) => {
+  if (memo[n] === undefined) {
+    if (n === 1) {
+      memo[n] = 1;
+    } else if (n % 2 === 0) {
+      memo[n] = collaz_seq(n / 2) + 1;
+    } else {
+      memo[n] = collaz_seq((3 * n + 1) / 2) + 2;
+    }
+  }  
+  return memo[n];
+};
 
-for(let i=1; i<1000000;i++) {
-  r = result(i);
-  if (r > largest) { 
-    largest = r ;
+let largest = 0,
+  number = 0,
+  r = 0;
+
+for (let i = 1; i < 1000000; i++) {
+  r = collaz_seq(i);
+  if (r > largest) {
+    largest = r;
     number = i;
   }
 }
-var t1 = Date.now()
+
+var t1 = Date.now();
 console.log('Took', (t1 - t0).toFixed(4), 'milliseconds to generate:', 'Number: '+ number, 'Largest Length: '+ largest);
+
