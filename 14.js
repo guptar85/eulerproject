15e8125e468435bea2b5ae29b4f8a9017f91da2b
= <rahul.gupta4@emc.com>
2020-05-10 13:33:58 +0530

    Solved Euler 14

--- 14.js
+++ 14.js
@@ -1,49 +1,27 @@
-var t0 = Date.now();
+console.time("Time taken");
 
-const cache = {};
+const longestChainNum = (MAX_NUMBER) => {
+    const cache = {};                                         // Cache object to stored the calculated values
+    const getCount = n =>                                     // Recursive Function to fill cache and get length
+        cache[n] === undefined                                // No value stored in cache
+        ? n === 1                                             // Last call in recurrsive function
+            ? (cache[n] = 1)                                  // Set cache as 1
+            : n % 2 === 0                                     // Check for Even or Odd
+                ? (cache[n] = getCount(n / 2) + 1)            // Store the value
+                : (cache[n] = getCount((3 * n + 1) / 2) + 2)  // Add 2 values since odd number will always result in even
+        : cache[n];
 
-const collaz_seq = n =>
-  cache[n] === undefined                               // No value stored in cache
-    ? n === 1                                          // Last call in recurrsive function
-      ? (cache[n] = 1)                                 // Set cache as 1
-      : n % 2 === 0                                    // Check for Even or Odd
-        ? (cache[n] = collaz_seq(n / 2) + 1)           // Store the value 
-        : (cache[n] = collaz_seq((3 * n + 1) / 2) + 2) // 
-    : cache[n];
-
-/* 
-const memo = {};
-
-if (memo[n] === undefined) {
-    if (n === 1) {
-      memo[n] = 1;
-    } else if (n % 2 === 0) {
-      memo[n] = collaz_seq(n / 2) + 1;
-    } else {
-      memo[n] = collaz_seq((3 * n + 1) / 2 ) + 2;
-    }
-  }  
-  return memo[n];
+    return ((i = 1) => {                                      //IIFE for loop and get the largest number
+        let largest     = 0,
+            number      = 0;
+        while (i++ < MAX_NUMBER) 
+            number = getCount(i) > largest 
+                     ? (largest = getCount(i)) && i 
+                     : number;
+        return number;
+    })();
 };
-*/
 
-let largest = 0,
-  number = 0,
-  r = 0;
+console.log(longestChainNum(1e6));
 
-for (let i = 1; i < 1000000; i++) {
-  r = collaz_seq(i);                                        
-  if (r > largest) {
-    largest = r;
-    number = i;
-  }
-}
-
-var t1 = Date.now();
-console.log(
-  "Took",
-  (t1 - t0).toFixed(4),
-  "milliseconds to generate:",
-  "Number: " + number,
-  "Largest Length: " + largest
-);
+console.timeEnd("Time taken");
